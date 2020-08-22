d9ba7f736797fac8cc9c4fc0484e54f2cdc3941e
Gwanwoo Kim <gwkgwk1125@naver.com>
2020-08-22 11:57:46 +0900

    Update ViewController.swift

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction  func silderValueChanged (_ sender : UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton (_ sender:UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton (_ sender: UIButton) {
+        slider.value = 15
+    }
 }
 
