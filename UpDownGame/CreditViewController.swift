0bdcaeebe8885aaff9eb02ad96baed18c1a7b381
Gwanwoo Kim <gwkgwk1125@naver.com>
2020-08-27 07:52:45 +0900

    Create CreditViewController.swift

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by wantap on 2020/08/26.
+//  Copyright © 2020 wantap. All rights reserved.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpDismissButton() {
+        dismiss(animated: true,
+                completion: nil)
+        
+    }
+}
